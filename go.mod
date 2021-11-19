842f8e8e56007282b750a8bc1922e76736825781
uglo1 <yujiro.takahashi0912@gmail.com>
2021-11-20 01:29:19 +0900

    first commit

---
+++ go.mod
@@ -0,0 +1,14 @@
+module go-tech-blog
+
+go 1.16
+
+require (
+	github.com/flosch/pongo2 v0.0.0-20200913210552-0d938eb266f3
+	github.com/go-sql-driver/mysql v1.6.0
+	github.com/jmoiron/sqlx v1.3.4
+	github.com/labstack/echo/v4 v4.6.1
+	github.com/lib/pq v1.10.4 // indirect
+	github.com/mattn/go-colorable v0.1.11 // indirect
+	github.com/mattn/go-sqlite3 v1.14.9 // indirect
+	golang.org/x/sys v0.0.0-20211117180635-dee7805ff2e1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"go-tech-blog/handler"
+	"go-tech-blog/repository"
+
+	_ "github.com/go-sql-driver/mysql" // Using MySQL driver
+	"github.com/jmoiron/sqlx"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+var db *sqlx.DB
+var e = createMux()
+
+func main() {
+	db = connectDB()
+	repository.SetDB(db)
+
+	e.GET("/", handler.ArticleIndex)
+	e.GET("/new", handler.ArticleNew)
+	e.GET("/:id", handler.ArticleShow)
+	e.GET("/:id/edit", handler.ArticleEdit)
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
+// DB接続
+func connectDB() *sqlx.DB {
+	dsn := os.Getenv("DSN")
+	db, err := sqlx.Open("mysql", dsn)
+	if err != nil {
+		e.Logger.Fatal(err)
+	}
+	if err := db.Ping(); err != nil {
+		e.Logger.Fatal(err)
+	}
+	log.Println("db connection succeeded")
+	return db
+}
+
+func createMux() *echo.Echo {
+	e := echo.New()
+
+	e.Use(middleware.Recover())
+	e.Use(middleware.Logger())
+	e.Use(middleware.Gzip())
+
+	e.Static("/css", "src/css")
+	e.Static("/js", "src/js")
+
+	return e
+}
